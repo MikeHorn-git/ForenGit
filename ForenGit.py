4188c5737b004244cc3d3da62689bf42ceb4fa6a
MikeHorn-git <123373126+MikeHorn-git@users.noreply.github.com>
2024-02-29 23:55:33 +0000

    Add files via upload

--- ForenGit.py
+++ ForenGit.py
@@ -25,40 +25,50 @@
 #################################################################################
 
 import argparse
-import csv
-import json
 import re
 import shutil
 import sys
 import subprocess
 from pathlib import Path
 
+
 def is_git_repository(path='.'):
     git_dir = Path(path) / '.git'
     if git_dir.exists() and git_dir.is_dir():
         return True
     else:
-        print(f'FATAL ERROR: {Path(path).resolve()} is not a Git repository')
+        print(f"FATAL ERROR: {Path(path).resolve()} is not a Git repository")
         sys.exit(1)
+
 
 def run_command(command):
     result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
     return result
 
-def git_author():
-    command = "git log --format='%aN' | sort -u"
 
-    result = subprocess.run(command, shell=True, capture_output=True, text=True)
-    return result.stdout
+def git_author():
+    git_log_command = ['git', 'log', '--format=%aN']
+    sort_command = ['sort', '-u']
+
+    git_process = subprocess.Popen(git_log_command, stdout=subprocess.PIPE)
+    sort_process = subprocess.Popen(sort_command, stdin=git_process.stdout, stdout=subprocess.PIPE)
+
+    git_process.stdout.close()
+
+    result, _ = sort_process.communicate()
+
+    return result.decode('utf-8')
+
 
 def git_check():
     command = ['git', 'fsck', '--full', '--unreachable', '--lost-found']
     result = run_command(command).stdout.strip()
-    
+
     if result:
         return result
     else:
         return "No issue found"
+
 
 def git_emails(repository_path='.'):
     command = ['git', 'log', '--pretty=format:%ae %ce']
@@ -71,10 +81,12 @@ def git_emails(repository_path='.'):
 
     return emails
 
+
 def git_geolocation():
     command = ['git', 'grep -E', "'[-+]?([1-8]?\d(\.\d+)?|90(\.0+)?)\s*[,]\s*[-+]?(180(\.0+)?|((1[0-7]\d)|([1-9]?\d))(\.\d+)?)'", '--', "'*.txt'", "'*.md'", "'*.json'"]
 
     return run_command(command).stdout
+
 
 def git_gpg_keys():
     all_gpg_keys = []
@@ -111,6 +123,21 @@ def git_gpg_keys():
 
     return all_gpg_keys
 
+
+def git_history_blame():
+    ls_files_command = ['git', 'ls-files']
+    xargs_command = ['xargs', '-I', '{}', 'git', 'blame', '--pretty=format:%h - %an %ad : %s', '--date=iso', '{}']
+
+    ls_files_process = subprocess.Popen(ls_files_command, stdout=subprocess.PIPE)
+    xargs_process = subprocess.Popen(xargs_command, stdin=ls_files_process.stdout, stdout=subprocess.PIPE)
+
+    ls_files_process.stdout.close()
+
+    result, _ = xargs_process.communicate()
+
+    return result.decode('utf-8', errors='replace')
+
+
 def git_history_branches():
     command = ['git', 'for-each-ref', '--sort=-committerdate', '--format', '%(refname:short) %(committername) %(committerdate:short) %(committerdate:relative)']
 
@@ -129,17 +156,12 @@ def git_history_branches():
 
     return branches
 
-def git_history_blame():
-    command = "git ls-files | xargs -I {} git blame --pretty=format:'%h - %an %ad : %s' --date=iso {}"
-    
-    result = subprocess.run(command, shell=True, capture_output=True)
-
-    return result.stdout.decode("utf-8", errors="replace")
 
 def git_history_commits():
     command = ['git', 'log', '--pretty=format:%h - %an, %ad : %s', '--date=iso']
 
     return run_command(command).stdout
+
 
 def git_history_deleted():
     command = ['git', 'log', '--diff-filter=D', '--name-only', '--pretty=format:%h - %an, %ad : %s']
@@ -151,6 +173,7 @@ def git_history_deleted():
 
     return formatted_commits
 
+
 def git_history_tags():
     command = ['git', 'for-each-ref', '--sort=-taggerdate', '--format', '%(refname:short) %(taggername) %(taggerdate:iso)']
 
@@ -160,18 +183,19 @@ def git_history_tags():
     for line in result.strip().split('\n'):
         parts = line.split()
         tag_name = parts[0]
-        tagger_name = parts[1] if len(parts) > 1 else 'Unknown Tagger'
-        creation_date = parts[2] if len(parts) > 2 else 'Unknown Date'
+        tagger_name = parts[1] if len(parts) > 1 else "Unknown Tagger"
+        creation_date = parts[2] if len(parts) > 2 else "Unknown Date"
         tags.append(f"{tag_name} - {tagger_name}, {creation_date}")
 
     return tags
+
 
 def git_network():
     command_ip = ['git', 'grep', '-E', '--ignore-case', r'\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b']
     result_ip = run_command(command_ip).stdout
 
     if result_ip:
-        print(f'Possible IP addresses found: {result_ip}')
+        print(f"Possible IP addresses found:\n{result_ip}")
         print("---------------------")
     else:
         print("No IP addresses found")
@@ -181,7 +205,7 @@ def git_network():
     result_mac = run_command(command_mac).stdout
 
     if result_mac:
-        print(f'Possible MAC addresses found: {result_mac}')
+        print(f"Possible MAC addresses found:\n{result_mac}")
         print("---------------------")
     else:
         print("No MAC addresses found")
@@ -191,44 +215,54 @@ def git_network():
     result_ssh = run_command(command_ssh).stdout
 
     if result_ssh:
-        print(f'Possible ssh information found: {result_ssh}')
+        print(f"Possible ssh information found:\n{result_ssh}")
     else:
         print("No SSH information found")
+
 
 def git_statistic():
     command = ['git', 'shortlog', '-s', '-n']
     return run_command(command).stdout
 
+
 def exif():
-    tool = ["exiftool"]
+    tool = ['exiftool']
 
     if shutil.which(tool[0]) is None:
         print(f"Error: {tool[0]} command not found.")
         return None
 
-    command = "git ls-files | grep -E '\.(gif|jpg|jpeg|png|tif|wav|webp)$' | xargs -I {} exiftool {}"
+    git_process = subprocess.Popen(['git', 'ls-files'], stdout=subprocess.PIPE)
+    grep_process = subprocess.Popen(['grep', '-E', '\.(gif|jpg|jpeg|png|tif|wav|webp)$'], stdin=git_process.stdout, stdout=subprocess.PIPE)
+    exif_process = subprocess.Popen(['xargs', '-I', '{}', 'exiftool', '{}'], stdin=grep_process.stdout, stdout=subprocess.PIPE)
 
-    result = subprocess.run(command, shell=True, capture_output=True, text=True)
-    return result.stdout
+    git_process.stdout.close()
+    grep_process.stdout.close()
+
+    result, _ = exif_process.communicate()
+
+    return result.decode('utf-8')
+
 
 def trivy():
-    tool = ["trivy"]
+    tool = ['trivy']
 
     if shutil.which(tool[0]) is None:
         print(f"FATAL ERROR: {tool[0]} command not found.")
         return None
 
-    command = ['trivy', 'repository', '.'] 
+    command = ['trivy', 'repository', '.']
     return run_command(command).stdout
 
+
 def virustotal():
-    tool = ["vt"]
+    tool = ['vt']
 
     if shutil.which(tool[0]) is None:
         print(f"FATAL ERROR: {tool[0]} command not found.")
         return None
 
-    command =  ['git', 'ls-files']
+    command = ['git', 'ls-files']
     result = run_command(command)
     files = result.stdout.split('\n')
     found_match = False
@@ -240,7 +274,7 @@ def virustotal():
             file_hash = result.stdout.strip()
             vt_command = ['vt', 'file', file_hash]
             vt_result = run_command(vt_command)
-            
+
             if vt_result.stdout:
                 print(f"Results for {file}:")
                 print(vt_result.stdout)
@@ -250,8 +284,9 @@ def virustotal():
     if not found_match:
         print("No matches found")
 
+
 def visualize():
-    tool = ["gource"]
+    tool = ['gource']
 
     if shutil.which(tool[0]) is None:
         print(f"FATAL ERROR: {tool[0]} command not found.")
@@ -260,21 +295,6 @@ def visualize():
     command = ['gource']
     return run_command(command)
 
-def export_data(data, filename=None, file_type=None):
-    if filename:
-        if file_type == 'csv':
-            with open(filename, 'w', newline='') as csv_file:
-                csv_writer = csv.writer(csv_file)
-                for entry in data.split('\n'):
-                                        csv_writer.writerow([entry.strip()])
-            print(f'Data exported to {filename} in CSV format')
-        elif file_type == 'json':
-            with open(filename, 'w') as json_file:
-                json.dump(data, json_file, indent=4)
-            print(f'Data exported to {filename} in JSON format')
-    else:
-        for entry in data:
-            print(entry)
 
 def main():
     is_git_repository()
@@ -284,7 +304,6 @@ def main():
     parser.add_argument('-e', '--emails', action='store_true', help='Display emails')
     parser.add_argument('-x', '--exif', action='store_true', help='Display exif metadata')
     parser.add_argument('-g', '--geolocation', action='store_true', help='Display latitude and longitude data')
-    parser.add_argument('-ha', '--history-all', action='store_true', help='Display all Git history')
     parser.add_argument('-hbl', '--history-blame', action='store_true', help='Display Git history branches')
     parser.add_argument('-hbr', '--history-branches', action='store_true', help='Display Git history branches')
     parser.add_argument('-hc', '--history-commits', action='store_true', help='Display Git history commits')
@@ -295,27 +314,23 @@ def main():
     parser.add_argument('-s', '--statistic', action='store_true', help='Display commits numbers by author')
     parser.add_argument('-t', '--trivy', action='store_true', help='Run Trivy')
     parser.add_argument('-vt', '--virustotal', action='store_true', help='Run Virustotal')
-    parser.add_argument('-vi', '--visualize', action='store_true', help='Run Gource') 
-    parser.add_argument('--csv', metavar='filename.csv', help='Export data to CSV file for history commands only')
-    parser.add_argument('--json', metavar='filename.json', help='Export data to JSON file for history commands only')
+    parser.add_argument('-vi', '--visualize', action='store_true', help='Run Gource')
 
     args = parser.parse_args()
 
     if not any(vars(args).values()):
         parser.print_help()
-    elif (args.csv or args.json) and not any([args.history_all, args.history_blame, args.history_branches, args.history_commits, args.history_deleted, args.history_tags]):
-        print("FATAL ERROR: --csv or --json must be used with history command")
     else:
-        data = []
- 
         if args.author:
-            data.append(git_author())
+            author_output = git_author()
+            if git_author():
+                print(author_output)
 
         if args.check:
             check_output = git_check()
             if check_output:
                 print(check_output)
- 
+
         if args.emails:
             emails_output = git_emails()
             if emails_output:
@@ -323,7 +338,7 @@ def main():
                     print(f"{email}")
             else:
                 return print("No emails found")
-        
+
         if args.exif:
             exif_output = exif()
             if exif_output:
@@ -334,82 +349,37 @@ def main():
         if args.geolocation:
             geolocation_output = git_geolocation()
             if geolocation_output:
-                print(f'Possible geolocation data found: {geolocation_output}')
+                print(f"Possible geolocation data found: {geolocation_output}")
             else:
                 print("No geolocation data found")
 
-        if args.history_all:
-            if git_history_blame():
-                print("Blame:")
-                print(f'{git_history_blame()}')
-
-            if git_history_branches():
-                print("Branches:")
-                for branch in git_history_branches():
-                    print(f'{branch}')
-
-            if git_history_commits():
-                print("Commits:")
-                print(f'{git_history_commits()}\n')
-            
-            if git_history_deleted():
-                print("Deleted:")
-                print(f'{git_history_deleted()}\n')
-
-            if git_history_tags():
-                print("Tags:")
-                for tag in git_history_tags():
-                    print(f'{tag}')
-        
         if args.history_blame:
             if git_history_blame():
-                if args.csv:
-                    export_data(git_history_blame(), args.csv, 'csv')
-                elif args.json:
-                    export_data(git_history_blame(), args.json, 'json')
-                else:
-                    print(git_history_blame()) 
+                print(git_history_blame())
 
         if args.history_branches:
             if git_history_branches():
-                if args.csv:
-                    export_data(git_history_branches(), args.csv, 'csv')
-                elif args.json:
-                    export_data(git_history_branches(), args.json, 'json')
-                else:
-                    print(git_history_branches()) 
+                for history in git_history_branches():
+                    print(history)
 
         if args.history_commits:
             if git_history_commits():
-                if args.csv:
-                    export_data(git_history_commits(), args.csv, 'csv')
-                elif args.json:
-                    export_data(git_history_commits(), args.json, 'json')
-                else:
-                    print(git_history_commits())
+                print(git_history_commits())
 
         if args.history_deleted:
             if git_history_deleted():
-                if args.csv:
-                    export_data(git_history_deleted(), args.csv, 'csv')
-                elif args.json:
-                    export_data(git_history_deleted(), args.json, 'json')
-                else:
-                    print(git_history_deleted())
+                print(git_history_deleted())
 
         if args.history_tags:
-            if git_history_tags():
-                if args.csv:
-                    export_data(git_history_tags(), args.csv, 'csv')
-                elif args.json:
-                    export_data(git_history_tags(), args.json, 'json')
-                else:
-                    print(git_history_tags())
+            tags_output = git_history_tags()
+            if tags_output:
+                for tag in tags_output:
+                    print(tag)
 
         if args.keys:
             gpg_output = git_gpg_keys()
             if gpg_output:
-                print(f'{gpg_output}')
+                print(f"{gpg_output}")
             else:
                 print("No gpg keys found")
 
@@ -419,18 +389,18 @@ def main():
         if args.statistic:
             statistic_output = git_statistic()
             if statistic_output:
-                print(f'{statistic_output}')
+                print(f"{statistic_output}")
 
         if args.trivy:
             trivy_output = trivy()
             if trivy_output:
-                print(f'{trivy_output}')
-        
+                print(f"{trivy_output}")
+
         if args.virustotal:
             virustotal()
 
         if args.visualize:
             visualize()
- 
+
 if __name__ == "__main__":
     main()
